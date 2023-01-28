7ddb0eb2811ced0bdbe71f27f4d3354e7f0486c6
Aviv Issachar <Avivissachar73@gmail.com>
2023-01-28 15:36:28 +0200

    env vars fix

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,19 @@
 const interfaceService = require('../services/interface.service');
 const configInterface = require('./config.scheme');
 
-const prodConf = {
+const devConf = {
   db: {
-    name: 'DB_NAME',
-    url: 'DB_URL'
+    name: 'ED',
+    url: 'mongodb+srv://ED_ADMIN:ED_APP_12345@ed.39otwpx.mongodb.net/?retryWrites=true&w=majority'
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'some_secret_string',
+    options: {
+      expiresIn: process.env.JWT_EXPIRES || '5h',
+      algorithm: process.env.JWT_ALGORITHM || 'HS256'
+    }
   }
 }
 
-interfaceService.validateType(configInterface, prodConf, true);
-module.exports = prodConf
+interfaceService.validateType(configInterface, devConf, true);
+module.exports = devConf
