32a89d546d4c56fff0634c0866f1a24344920e1b
Aviv Issachar <Avivissachar73@gmail.com>
2023-01-28 16:00:39 +0200

    fixed dynamic routes import

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const { connectAccountRoutes } = require('../api/account/account.routes');
 const { connectAuthRoutes } = require('../api/auth/auth.routes');
 const { connectActivityRoutes } = require('../api/activity/activity.routes');
 const { connectSettingsRoutes } = require('../api/settings/settings.routes');
-const { connectDynamicDbRoutes } = require('../api/DynamicDb/DynamicDb.routes');
+const { connectDynamicDbRoutes } = require('../api/dynamicDb/dynamicDb.routes');
 const { connectOrganizationRoutes } = require('../api/organization/organization.routes');
 
 const { connectPostRoutes } = require('../api/post/post.routes');
