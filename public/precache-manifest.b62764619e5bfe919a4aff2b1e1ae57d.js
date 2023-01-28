42ddf3f9a5259b4146111e72831550182eb4e983
Aviv Issachar <Avivissachar73@gmail.com>
2023-01-28 13:42:03 +0200

    build

---
+++ public/precache-manifest.b62764619e5bfe919a4aff2b1e1ae57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32308e77beb20876440a",
+    "url": "/css/app.7831ec16.css"
+  },
+  {
+    "revision": "c5045d7277acc696981864597e292f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32308e77beb20876440a",
+    "url": "/js/app.a95a9dcf.js"
+  },
+  {
+    "revision": "9804f378ad279447f580",
+    "url": "/js/chunk-vendors.32cfb1ad.js"
+  },
+  {
+    "revision": "82db4b82a5a74e835be5e8cb69f27350",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
